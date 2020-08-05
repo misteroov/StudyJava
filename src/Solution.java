f06015f4ef0696e3448e57d1f440a7be2b97f6be
misteroov <misteroov@yandex.ua>
2020-08-05 17:33:17 +0300

    javarush Новая задача

--- src/Solution.java
+++ src/Solution.java
@@ -1,88 +1,45 @@
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Scanner;
+
 
 public class Solution {
-    public static String firstFileName;
-    public static String secondFileName;
+    public static Thread.UncaughtExceptionHandler handler = new OurUncaughtExceptionHandler();
 
-static {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    try {
-        firstFileName =reader.readLine();
-        secondFileName=reader.readLine();
-    } catch (IOException e) {
-        e.printStackTrace();
+    public static void main(String[] args) {
+        TestedThread commonThread = new TestedThread(handler);
+
+        Thread threadA = new Thread(commonThread, "Нить 1");
+        Thread threadB = new Thread(commonThread, "Нить 2");
+
+        threadA.start();
+        threadB.start();
+
+        threadA.interrupt();
+        threadB.interrupt();
     }
 
-}
-    //add your code here - добавьте код тут
-
-    public static void main(String[] args) throws InterruptedException {
-
-        systemOutPrintln(firstFileName);
-        systemOutPrintln(secondFileName);
-    }
-
-    public static void systemOutPrintln(String fileName) throws InterruptedException {
-        ReadFileInterface f = new ReadFileThread();
-        f.setFileName(fileName);
-        f.start();
-        f.join();
-        //add your code here - добавьте код тут
-        System.out.println(f.getFileContent());
-    }
-
-    public interface ReadFileInterface {
-
-        void setFileName(String fullFileName);
-
-        String getFileContent();
-
-        void join() throws InterruptedException;
-
-        void start();
-    }
-public static class ReadFileThread  extends Thread implements ReadFileInterface{
-  ArrayList <String> list = new ArrayList<>();
-  String fileName;
-
-    @Override
-    public void setFileName(String fullFileName) {
-       fileName  = fullFileName;
-
-    }
-
-    @Override
-    public String getFileContent() {
-
-        StringBuilder sb = new StringBuilder();
-
-        for (String s : list) {
-            sb.append(s + " ");
+    public static class TestedThread extends Thread {
+        public TestedThread(Thread.UncaughtExceptionHandler handler) {
+            setUncaughtExceptionHandler(handler);
+            start();
         }
 
-        String finalLine = sb.toString();
-        return finalLine;
+        public void run() {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException("My exception message");
+            }
+        }
     }
-    public void run(){
-       try  {
-           Scanner sc = new Scanner(new File(fileName));
-           while (sc.hasNextLine()){
-               String line = sc.nextLine();
-               list.add(line);
-           }
-           sc.close();
-       }catch (IOException e){
-           e.printStackTrace();
-       }
-        //fsdfsd
+
+    public static class OurUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
+        @Override
+        public void uncaughtException(Thread t, Throwable e) {
+            System.out.println(t.getName() + ": " + e.getMessage());
+        }
     }
-}
-    //add your code here - добавьте код тут
 }
 
-                                                  
+
                                                   
                                                   
                                                   
